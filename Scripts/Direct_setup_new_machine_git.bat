b24ec072d54facad997f9e5e84b18d9fb6bbe291
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-18 19:06:45 +0100

    Seperate bat for script starter

---
+++ Scripts/Direct_setup_new_machine_git.bat
@@ -0,0 +1,30 @@
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. 
+	echo -----------------------------------------
+	echo Detecting permissions...
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+		echo -------------------------------------
+    ) else (
+        echo Failure: Current permissions inadequate.
+		echo -------------------------------------
+		echo Run as administrator and try again.
+		pause
+		exit
+    )
+cls
+mkdir c:\temp
+bitsadmin /transfer myDownloadJob /download /priority normal "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/Setup_new_machine.ps1" c:\temp\setup_new_machine.ps1
+powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
+powershell c:\temp\setup_new_machine.ps1
+cls
+echo Only press enter if you are done.
+powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Restricted -Force}"
+pause
+del c:\temp\setup_new_machine.ps1
+exit
+
