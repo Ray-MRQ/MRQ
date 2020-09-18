a525f6d9d716035eb34f86198fd00d93ac685c87
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-18 13:41:37 +0100

    Added default exection policy into end of script.
    Added default exection policy into end of script.

--- Scripts/Direct_setup_new_machine.bat
+++ Scripts/Direct_setup_new_machine.bat
@@ -17,18 +17,17 @@ goto check_Permissions
 		exit
     )
 cls
-echo Logon as administrator or this will error out.
 echo If your having trouble, download the file manually store in c:\temp and then run this again.
 echo "https://onl-my.sharepoint.com/:u:/g/personal/mohammed_quashem_onlinesupport_co_uk/EWjxSgJkryRAh3Pe3l8sGlsBqdWV1N1h6URgBhCvkdsmeg?e=0hcgyh&download=1"
 pause
+cls
 mkdir c:\temp
 bitsadmin /transfer myDownloadJob /download /priority normal "https://onl-my.sharepoint.com/:u:/g/personal/mohammed_quashem_onlinesupport_co_uk/EWjxSgJkryRAh3Pe3l8sGlsBqdWV1N1h6URgBhCvkdsmeg?e=0hcgyh&download=1" c:\temp\setup_new_machine.ps1
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
 powershell c:\temp\setup_new_machine.ps1
 cls
 echo Only press enter if you are done.
-echo (This batch script downloads a ps1 script and then runs it directly.)
-echo (Continuning only deletes the .ps1 script).
+powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Restricted -Force}"
 pause
 del c:\temp\setup_new_machine.ps1
 exit
