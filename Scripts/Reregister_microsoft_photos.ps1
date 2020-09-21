edc77aaceb68d56d6aed89dd16beef73892c428f
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-21 19:56:11 +0100

    Updated to use strings instead of requiring manual input.

--- Scripts/Reregister_microsoft_photos.ps1
+++ Scripts/Reregister_microsoft_photos.ps1
@@ -1,9 +1,4 @@
-﻿Get-AppxPackage Microsoft.Windows.Photos -allusers | select PackageFullName
-echo "------------------------------------------------------------"
-echo "Note down the above it is the package name for photo viewer"
-echo "------------------------------------------------------------"
-echo "Copy this line powershell command and adjust as needed (not that if you have a diffirent \Microsoft.Photos.XX then change it to the package name above)"
-echo "------------------------------------------------------------"
-echo "Add-AppxPackage -register C:\Program Files\WindowsApps\Microsoft.Windows.Photos_16.302.8200.0_x64__8wekyb3d8bbwe\AppxManifest.xml -DisableDevelopmentMode"
-echo "You need to add speach marks for the file path since it's got a space in it"
+﻿$Photos = Get-AppxPackage Microsoft.Windows.Photos -allusers | select PackageFullName | findstr /B Microsoft*
+Add-AppxPackage -register "C:\Program Files\WindowsApps\$Photos\AppxManifest.xml" -DisableDevelopmentMode
+echo "This only works if the photos app has only be uninstalled by one user or if the app hasn't been removed provisoned packages."
 pause
