2ab2f443efe15088d60904a06e4d193c092b0ef3
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-21 20:10:20 +0100

    Updated to make it easier to remove apps.

--- Scripts/Uninstall_windows10_bloatware_apps.ps1
+++ Scripts/Uninstall_windows10_bloatware_apps.ps1
@@ -2,42 +2,51 @@
 (  _ \(  )  (  _  )  /__\ (_  _)( \/\/ )  /__\  (  _ \( ___)  (  _ \( ___)(  \/  )(  _  )( \/ )/__\  (  )  
  ) _ ( )(__  )(_)(  /(__)\  )(   )    (  /(__)\  )   / )__)    )   / )__)  )    (  )(_)(  \  //(__)\  )(__ 
 (____/(____)(_____)(__)(__)(__) (__/\__)(__)(__)(_)\_)(____)  (_)\_)(____)(_/\/\_)(_____)  \/(__)(__)(____)"
+echo "Start script? If you want to exit close the prompt, this is only for per user basis."
 pause
+$AppList = "Microsoft.SkypeApp",          
+           "Microsoft.ZuneMusic",
+           "Microsoft.ZuneVideo",
+           "Microsoft.Office.OneNote",
+           "Microsoft.BingFinance",
+           "Microsoft.BingNews",
+           "Microsoft.BingWeather",
+           "Microsoft.BingSports",
+           "Microsoft.XboxApp",
+           "Microsoft.MicrosoftOfficeHub",
+		   "Microsoft.Wallet",
+		   "Microsoft.OneConnect",
+		   "Microsoft.MSPaint",
+		   "Microsoft.Print3D",
+		   "Microsoft.Messaging",
+		   "Microsoft.Microsoft3DViewer",
+		   "Microsoft.Windows.Cortana",
+		   "Microsoft.3DBuilder",
+		   "Microsoft.WindowsAlarms",
+		   "Microsoft.windowscommunicationsapps",
+		   "Microsoft.Getstarted",
+		   "Microsoft.WindowsMaps",
+		   "Microsoft.MicrosoftSolitaireCollection",
+		   "Microsoft.WindowsFeedbackHub",
+		   "Microsoft.MixedReality.Portal",
+		   "Microsoft.GetHelp",
+		   "Microsoft.People",
+		   "Microsoft.549981C3F5F10",
+		   "Microsoft.549981cf5f10",
+		   "Microsoft.Xbox.TCUI",
+		   "Microsoft.XboxSpeechToTextOverlay",
+		   "Microsoft.XboxGamingOverlay",
+		   "Microsoft.XboxGameOverlay"
+ForEach ($App in $AppList)
+ {
+ $PackageFullName = (Get-AppxPackage $App).PackageFullName
+ if ($PackageFullName)
+ {
+ Write-Host "Removing Package: $App"
+ remove-AppxPackage -package $PackageFullName 
+ }
+ }
 cls
-echo " ____  __    _____    __   ____  _    _    __    ____  ____    ____  ____  __  __  _____  _  _  __    __   
-(  _ \(  )  (  _  )  /__\ (_  _)( \/\/ )  /__\  (  _ \( ___)  (  _ \( ___)(  \/  )(  _  )( \/ )/__\  (  )  
- ) _ ( )(__  )(_)(  /(__)\  )(   )    (  /(__)\  )   / )__)    )   / )__)  )    (  )(_)(  \  //(__)\  )(__ 
-(____/(____)(_____)(__)(__)(__) (__/\__)(__)(__)(_)\_)(____)  (_)\_)(____)(_/\/\_)(_____)  \/(__)(__)(____)"
-Get-AppxPackage Microsoft.Print3D | Remove-AppxPackage
-Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage
-Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage
-Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
-Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage
-Get-AppxPackage "Microsoft.3DBuilder" | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsAlarms | Remove-AppxPackage
-Get-AppxPackage Microsoft.windowscommunicationsapps | Remove-AppxPackage
-Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
-Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
-Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
-Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
-Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
-Get-AppxPackage *bingfinance* | Remove-AppxPackage
-Get-AppxPackage *bingnews* | Remove-AppxPackage
-Get-AppxPackage *bingsports* | Remove-AppxPackage
-Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
-Get-AppxPackage -Name king.com.CandyCrushSaga | Remove-AppxPackage
-Get-AppxPackage Microsoft.WindowsFeedbackHub| Remove-AppxPackage
-Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
-Get-AppxPackage Microsoft.MixedReality.Portal | Remove-AppxPackage
-Get-AppxPackage Microsoft.MSPaint | Remove-AppxPackage
-Get-AppxPackage Microsoft.windowscommunicationsapps | Remove-AppxPackage
-Get-AppxPackage Microsoft.Microsoft3DViewer | Remove-AppxPackage
-Get-AppxPackage Microsoft.GetHelp | Remove-AppxPackage
-Get-AppxPackage Microsoft.People | Remove-AppxPackage
-Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage
-Get-AppxPackage Microsoft.Wallet | Remove-AppxPackage
-echo "=================================================================="
-echo "Uninstalls done. Please close the prompt."
-echo "=================================================================="
+echo "Please confirm it has removed applications."
 pause
+exit
