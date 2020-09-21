44e0527fa00c51d3320eae5ad107571dfb398765
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-21 19:35:13 +0100

    Used Invoke-Webrequest to download script instead of BITS

--- Scripts/Direct_setup_new_machine_git.bat
+++ Scripts/Direct_setup_new_machine_git.bat
@@ -18,9 +18,10 @@ goto check_Permissions
     )
 cls
 mkdir c:\temp
-bitsadmin /transfer myDownloadJob /download /priority normal "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/Setup_new_machine.ps1" c:\temp\setup_new_machine.ps1
+powershell -command Invoke-WebRequest "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/Setup_new_machine.ps1" -outfile c:\temp\setup_new_machine.ps1
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
 powershell c:\temp\setup_new_machine.ps1
+pause
 cls
 echo Only press enter if you are done.
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Restricted -Force}"
