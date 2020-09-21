ae0fe55b52e7daa2d1c65583a0878e4c1f76cb51
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-21 19:32:34 +0100

    Updated for ONL/General usage

--- Scripts/Check MFA on a Tenant.ps1
+++ Scripts/Check MFA on a Tenant.ps1
@@ -1,2 +1,4 @@
 ﻿Connect-MsolService
 Get-MsolUser -all | where {$_.isLicensed -eq $true} | select UserPrincipalName,@{N="MFA Status"; E={ if( $_.StrongAuthenticationMethods.IsDefault -eq $true) {($_.StrongAuthenticationMethods | Where IsDefault -eq $True).MethodType} else { "Disabled"}}} | Export-CSV -Path C:\Users\$env:USERNAME\Desktop\MFAReport.csv
+echo "Exported to C:\Users\$env:USERNAME\Desktop\MFAReport.csv"
+pause
