fdd6491cb911d09ccf0939aceece81543d2f02e3
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-25 17:29:07 +0100

    Add files via upload

---
+++ Scripts/Restart_or_Shutdown_a_pc.ps1
@@ -0,0 +1,22 @@
+echo "Restart/Shutdown a PC remotely"
+""
+
+$PCName = Read-Host -prompt 'Enter PC name'
+""
+$User = Read-Host -prompt 'Enter Administrator username for the remote PC in Domain\Username format'
+
+""
+do { $myInput = (Read-Host 'Restart or shutdown a remote PC? (restart/shutdown/exit)').ToLower() } while ($myInput -notin @('restart','shutdown','exit'))
+if ($myinput -eq 'restart') { 
+""
+echo "Restarting remote PC if possible..."
+""
+runas /noprofile /user:$user "powershell shutdown /r /m \\$PCName /t 0"
+}
+if ($myinput -eq 'shutdown') {
+runas /noprofile /user:$user "powershell shutdown /s /m \\$PCName"
+}
+if ($myinput -eq 'exit') {
+exit
+}
+pause
