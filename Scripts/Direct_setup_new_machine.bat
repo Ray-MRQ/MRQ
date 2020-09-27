a4c252347740461281afcca9f311f43264bf49f4
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-27 15:09:31 +0100

    Removed non existent goto

--- Scripts/Direct_setup_new_machine.bat
+++ Scripts/Direct_setup_new_machine.bat
@@ -1,5 +1,4 @@
 @echo off
-goto check_Permissions
 
 net file 1>nul 2>nul && goto :run || powershell -ex unrestricted -Command "Start-Process -Verb RunAs -FilePath '%comspec%' -ArgumentList '/c %~fnx0 %*'"
 goto :eof
