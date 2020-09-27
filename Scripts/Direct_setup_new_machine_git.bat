c1d24562533c4151f086e49bcadcb075b96cc460
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-27 11:44:30 +0100

    Force script to run as administrator

--- Scripts/Direct_setup_new_machine_git.bat
+++ Scripts/Direct_setup_new_machine_git.bat
@@ -1,21 +1,9 @@
 @echo off
-goto check_Permissions
 
-:check_Permissions
-    echo Administrative permissions required. 
-	echo -----------------------------------------
-	echo Detecting permissions...
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        echo Success: Administrative permissions confirmed.
-		echo -------------------------------------
-    ) else (
-        echo Failure: Current permissions inadequate.
-		echo -------------------------------------
-		echo Run as administrator and try again.
-		pause
-		exit
-    )
+net file 1>nul 2>nul && goto :run || powershell -ex unrestricted -Command "Start-Process -Verb RunAs -FilePath '%comspec%' -ArgumentList '/c %~fnx0 %*'"
+goto :eof
+:run
+
 cls
 mkdir c:\temp
 powershell -command Invoke-WebRequest "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/Setup_new_machine.ps1" -outfile c:\temp\setup_new_machine.ps1
@@ -28,4 +16,3 @@ powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Restricted -Force}"
 pause
 del c:\temp\setup_new_machine.ps1
 exit
-
