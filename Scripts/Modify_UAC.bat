57e9f6722e725aa54b19e7c07fb6de0a4d0d5010
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-27 21:33:47 +0100

    Fixed an issue with it looping

--- Scripts/Modify_UAC.bat
+++ Scripts/Modify_UAC.bat
@@ -3,6 +3,7 @@
 net file 1>nul 2>nul && goto :run || powershell -ex unrestricted -Command "Start-Process -Verb RunAs -FilePath '%comspec%' -ArgumentList '/c %~fnx0 %*'"
 goto :eof
 :run
+
 echo Modifying UAC.
 
 :begin
@@ -13,15 +14,18 @@ echo 1) Turn on UAC
 echo 2) Use Dimmed UAC
 echo 3) Turn off UAC
 
+echo.
+echo.
+
+set /P c=Choose a option:
+
 if "%op%"=="1" goto op1
 if "%op%"=="2" goto op2
 if "%op%"=="3" goto op3
 
-echo Please pick a task
-goto begin
 
 :op1 
-
+cls
 reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
 echo May require a restart to take full effect.
 pause
@@ -29,7 +33,7 @@ cls
 goto begin
 
 :op2
-
+cls
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t reg_dword /d 0 /f
 echo May require a restart to take full effect.
 pause
@@ -37,7 +41,7 @@ cls
 goto begin
 
 :op3
-
+cls
 reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 echo May require a restart to take full effect.
 pause
