93834bcc14e2ed9757abbb33ce97504687f633c7
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-27 21:34:36 +0100

    Auto elevate

--- Scripts/Uninstall_onedrive.bat
+++ Scripts/Uninstall_onedrive.bat
@@ -1,5 +1,9 @@
 @echo off
 
+net file 1>nul 2>nul && goto :run || powershell -ex unrestricted -Command "Start-Process -Verb RunAs -FilePath '%comspec%' -ArgumentList '/c %~fnx0 %*'"
+goto :eof
+:run
+
 taskkill /f /im OneDrive.exe
 
 :choice
