38dc2e19b5f206214e8986044a88863e5145d782
Raÿ <ramim_qr@hotmail.co.uk>
2020-09-27 21:33:57 +0100

    Cleaner output

--- Scripts/Add_runas_administratorps1.bat
+++ Scripts/Add_runas_administratorps1.bat
@@ -6,6 +6,7 @@ goto :eof
 
 bitsadmin /transfer myDownloadJob /download /priority normal "https://onl-my.sharepoint.com/:u:/g/personal/mohammed_quashem_onlinesupport_co_uk/EUdAD4ToqqtOqD8oI6EeEycBZvLTSuK96di0MsuxoCTkvw?e=bDhL5E&download=1" c:\RunAsAdminContextMenupowershell.reg
 reg import C:\RunAsAdminContextMenupowershell.reg
+cls
 del "C:\RunAsAdminContextMenupowershell.reg"
 echo "Check now with a .ps1 file and see if it appears in the context menu"
 pause
