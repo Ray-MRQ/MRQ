3334ddd5d7d5762114fb384b2517a26692b894f5
Raÿ <ramim_qr@hotmail.co.uk>
2020-10-01 19:30:15 +0100

    Testing

--- Scripts/Setup_new_machine.ps1
+++ Scripts/Setup_new_machine.ps1
@@ -752,7 +752,7 @@ $webClient = New-Object System.Net.WebClient
 $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
 $file = "$($dir)\Win10Upgrade.exe"
 $webClient.DownloadFile($url,$file)
-Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'
+Start-Process -FilePath $file -ArgumentList '/skipeula /auto upgrade /copylogs $dir'
 ""
 start-windows-update-running-checker
 echo "Please re-run the bloatware remover after restarting as doing a feature update may add new bloatware back in."
