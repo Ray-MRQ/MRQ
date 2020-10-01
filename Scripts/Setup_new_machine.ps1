50112d4ebc046e557f81c5ef4db9b00ef7114a91
Raÿ <ramim_qr@hotmail.co.uk>
2020-10-01 19:58:04 +0100

    added pending reboot

--- Scripts/Setup_new_machine.ps1
+++ Scripts/Setup_new_machine.ps1
@@ -736,6 +736,11 @@ cl
 ""
 echo "Run this last, if you have an old verison of windows and a feature update is applicable it will not move from the status checker until it restarts."
 echo "If you enabled bitlocker and still haven't restart, do it now and then use manual select option 50 to come back here."
+""
+Install-PackageProvider -Name NuGet -Force -MinimumVersion 2.8.5.201 > $null 2>&1
+Import-Module PendingReboot -Force > $null 2>&1
+Test-PendingReboot -SkipConfigurationManagerClientCheck
+""
 pause
 cl
 ""
