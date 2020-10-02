aa390c6b9e2fb802abab7f2547eb2474640b97a2
Raÿ <ramim_qr@hotmail.co.uk>
2020-10-02 16:37:53 +0100

    added auto elevate

--- Scripts/Uninstall_windows10_bloatware_apps_all_users.ps1
+++ Scripts/Uninstall_windows10_bloatware_apps_all_users.ps1
@@ -1,3 +1,9 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
 $OriginalPref = $ProgressPreference # Default is 'Continue'
 $ProgressPreference = "SilentlyContinue"
 echo " ____  __    _____    __   ____  _    _    __    ____  ____    ____  ____  __  __  _____  _  _  __    __   
