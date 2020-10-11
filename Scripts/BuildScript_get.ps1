c598b2c227be4f99841c0bedbf599cedfc98bb78
Raÿ <ramim_qr@hotmail.co.uk>
2020-10-11 21:25:06 +0100

    Added self elevate

--- Scripts/BuildScript_get.ps1
+++ Scripts/BuildScript_get.ps1
@@ -1,3 +1,9 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
 mkdir c:\temp
 powershell -command Invoke-WebRequest "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/Setup_new_machine.ps1" -outfile c:\temp\setup_new_machine.ps1
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
