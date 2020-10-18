d7d0cdbedc15d05bb14d1dafd773a5e833b7eaca
Raÿ <ramim_qr@hotmail.co.uk>
2020-10-18 01:19:33 +0100

    update ver

--- Scripts/BuildScript.ps1
+++ Scripts/BuildScript.ps1
@@ -6,8 +6,8 @@
 }
 Clear-Host
 $createdby = Write-Output "Created By MQ 08/09/2020"
-$Version = Write-Output "Version 2.68"
-$lastupdatedby = Write-Output "Last Updated By MQ 14/10/2020"
+$Version = Write-Output "Version 1.30"
+$lastupdatedby = Write-Output "Last Updated By MQ 18/10/2020"
 
 $WindowsVerison = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
 $LatestWindows = '2004' #Current Windows verison
