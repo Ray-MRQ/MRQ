bf605306708e16d41972fa39ca1158aa68dcdd06
Raÿ <ramim_qr@hotmail.co.uk>
2020-10-18 01:59:34 +0100

    test

--- InstallExes/TemplatePS1_2_EXE.ps1
+++ InstallExes/TemplatePS1_2_EXE.ps1
@@ -1,12 +1,12 @@
-$File = "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/Install_Office365Apps.ps1"
+$File = "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/HP-Bloatware-removal.ps1"
 mkdir c:\temp > $null 2>&1
 mkdir c:\temp\scriptdownloads > $null 2>&1
 
-powershell -command Invoke-WebRequest "$File" -outfile c:\temp\InstallOffice365apps.ps1
+powershell -command Invoke-WebRequest "$File" -outfile c:\temp\HP-Bloatware-Removal.ps1
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
-powershell c:\temp\InstallOffice365apps.ps1
+powershell c:\temp\HP-Bloatware-Removal.ps1
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Restricted -Force}"
-Remove-Item c:\temp\InstallOffice365apps.ps1
+Remove-Item c:\temp\HP-Bloatware-Removal.ps1
 
 Remove-Item c:\temp\scriptdownloads -recurse -force > $null 2>&1
 exit
