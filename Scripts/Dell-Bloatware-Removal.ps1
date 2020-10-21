ef4c62af3f43ebb9125e376ed3c0bc27d471868e
Raÿ <ramim_qr@hotmail.co.uk>
2020-10-21 13:47:13 +0100

    guid fix x4

--- Scripts/Dell-Bloatware-Removal.ps1
+++ Scripts/Dell-Bloatware-Removal.ps1
@@ -72,7 +72,7 @@ Write-Output "Removed UWP apps."
 
 function start-WMIC-removal {
 ForEach ($App in $WMICAppList) {
-Start-Process -wait WMIC -ArgumentList '/MIN product where name=$App call uninstall /nointeractive'
+Start-Process -wait WMIC -ArgumentList 'product where name="$App" call uninstall /nointeractive'
 }            
 Write-Output "Removed apps using WMIC."
 }
