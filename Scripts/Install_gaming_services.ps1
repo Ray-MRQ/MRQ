63e01c0b3cad847aae6505a2df0a899dda4b1f3b
Raÿ <ramim_qr@hotmail.co.uk>
2020-11-12 09:36:19 +0000

    Add administrator run as

--- Scripts/Install_gaming_services.ps1
+++ Scripts/Install_gaming_services.ps1
@@ -1,4 +1,11 @@
-﻿Write-Outpute-Output "Make sure you run as administrator + accept any UAC prompts, hit enter to confirm"
+﻿if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+Write-Output "Make sure you run as administrator + accept any UAC prompts, hit enter to confirm"
 pause
 get-appxpackage Microsoft.GamingServices | remove-AppxPackage -allusers
 Start-Process ms-windows-store://pdp/?productid=9MWPM2CQNLHN
