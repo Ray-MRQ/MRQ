7f5d6000605d5ec4df5eebf70e54b0c48acd1fcf
Raÿ <ramim_qr@hotmail.co.uk>
2020-11-13 10:50:38 +0000

    fix to use commands insterad of alias.

--- Scripts/Uninstall_old_edge.ps1
+++ Scripts/Uninstall_old_edge.ps1
@@ -4,15 +4,15 @@
   Start-Process powershell -Verb runAs -ArgumentList $arguments
   Break
 }
-
-echo "Confirm if you would like to uninstall old edge"
+Clear-Host
+Write-Output "Confirm if you would like to uninstall old edge"
 pause
-cls
+Clear-Host
 Invoke-Webrequest https://github.com/Ray-MRQ/MRQ/raw/master/Install%20files/install_wim_tweak.exe -outfile .\install_wim_tweak.exe
-echo Uninstalling Microsoft Edge...
+Write-Output Uninstalling Microsoft Edge...
 install_wim_tweak.exe /o /l
 install_wim_tweak.exe /o /c Microsoft-Windows-Internet-Browser-Package /r
 install_wim_tweak.exe /h /o /l
-echo Microsoft Edge should be uninstalled. Please reboot Windows 10.
+Write-Output "Microsoft Edge should be uninstalled. Please reboot Windows 10."
 pause
-del .\install_wim_tweak.exe
+Remove-Item .\install_wim_tweak.exe
