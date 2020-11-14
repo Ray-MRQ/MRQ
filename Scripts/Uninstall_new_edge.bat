cc385fadb5cd96e182a1fde566ba19e4e37821c6
Raÿ <ramim_qr@hotmail.co.uk>
2020-11-14 16:57:04 +0000

    update edge removal scripts

--- Scripts/Uninstall_new_edge.bat
+++ Scripts/Uninstall_new_edge.bat
@@ -4,9 +4,14 @@ net file 1>nul 2>nul && goto :run || powershell -ex unrestricted -Command "Start
 goto :eof
 :run
 
+echo Please confirm you would like New Microsoft Edge to be removed.
+pause
+
 rmdir /q /s "C:\Program Files (x86)\Microsoft\Edge"
 rmdir /q /s "C:\Program Files (x86)\Microsoft\EdgeUpdate"
 rmdir /q /s "C:\Program Files (x86)\Microsoft\Temp"
+del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk"
+del "C:\Users\Public\Desktop\Microsoft Edge.lnk"
 cls
 
 echo This should now remove the new edge, you might need to delete from the Recyle bin as well
--- Scripts/Uninstall_old_edge.ps1
+++ Scripts/Uninstall_old_edge.ps1
@@ -13,6 +13,6 @@ Write-Output Uninstalling Microsoft Edge...
 install_wim_tweak.exe /o /l
 install_wim_tweak.exe /o /c Microsoft-Windows-Internet-Browser-Package /r
 install_wim_tweak.exe /h /o /l
-Write-Output "Microsoft Edge should be uninstalled. Please reboot Windows 10."
+Write-Output "Microsoft Edge should be uninstalled."
 pause
 Remove-Item .\install_wim_tweak.exe
