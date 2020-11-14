e472d965fecb2f6f53b4906be609d7edf14ccff1
Raÿ <ramim_qr@hotmail.co.uk>
2020-11-14 16:21:50 +0000

    create offiec365 shortcuts

--- Scripts/Centrastage_Office_Install.ps1
+++ Scripts/Centrastage_Office_Install.ps1
@@ -105,5 +105,8 @@ Write-Output "Office365 is now installed."
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 
 start-officecheck
-
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk" -Destination "C:\Users\Public\Desktop\Outlook.lnk"
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk" -Destination "C:\Users\Public\Desktop\Excel.lnk"
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Powerpoint.lnk" -Destination "C:\Users\Public\Desktop\Powerpoint.lnk"
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk" -Destination "C:\Users\Public\Desktop\Word.lnk"
 Remove-Item c:\temp\scriptdownloads -recurse -force > $null 2>&1 
