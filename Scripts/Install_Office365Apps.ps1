1c70902b0a3783e79e3db21beb75040ab2268c94
Mohammed Ramim Quashem <ramim_qr@mrqlab.co.uk>
2021-03-23 12:44:05 +0000

    Update Install_Office365Apps.ps1

--- Scripts/Install_Office365Apps.ps1
+++ Scripts/Install_Office365Apps.ps1
@@ -14,30 +14,9 @@ $OfficeXMLHomeUninstall = 'https://github.com/Ray-MRQ/MRQ/raw/master/Regkeys_xml
 $OfficeXMLBuisnessUninstall = 'https://github.com/Ray-MRQ/MRQ/raw/master/Regkeys_xmls/configuration_uninstall_buisness.xml'
 
 function start-officecheck {
-    $uninstallKeys = Get-ChildItem -Path "HKLM:HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-    $O365 = "Microsoft 365"
-    $O365Check = $uninstallKeys | Where-Object { $_.GetValue("DisplayName") -match $O365 }
-    
-    $uninstallKeysHome = Get-ChildItem -Path "HKLM:HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-    $O365Home = "Microsoft Office 365"
-    $O365CheckHome = $uninstallKeysHome | Where-Object { $_.GetValue("DisplayName") -match $O365Home }
-    
-    $O365Installed = Write-Output "Office 365 installation has been detected."
-    $O365NotInstalled = Write-Output "Office 365 is not installed."
-    if ($O365Check) {
-    $O365Installed
-    Write-Output ''
     start-officeuninstall-pro
     start-officeuninstall-buisness
-    start-officeinstall
-    }
-    if ($O365CheckHome) {
     start-officeuninstall-home
-    start-officeinstall
-    }
-    else {
-    $O365NotInstalled
-    Write-Output ''
     start-officeinstall
     }
 }
