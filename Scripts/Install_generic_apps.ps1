c78d81e86950e66b39549c56f6c88fe3a5d6174a
Mohammed Ramim Quashem <ramim_qr@mrqlab.co.uk>
2021-04-16 12:20:38 +0100

    remove -uri

--- Scripts/Install_generic_apps.ps1
+++ Scripts/Install_generic_apps.ps1
@@ -1,10 +1,10 @@
 Write-Output ''
 Write-Output "Starting download and install for 7Zip, Java, Chrome & Adobe Reader..."
 $ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri $SoftwareInstallChrome -outfile c:\temp\downloads\chrome.msi -Verbose
-Invoke-WebRequest -Uri $SoftwareInstall7zip -outfile c:\temp\downloads\7zip.msi -Verbose
-Invoke-WebRequest -Uri $SoftwareInstallJava -outfile c:\temp\downloads\java.msi -Verbose
-Invoke-WebRequest -Uri $SoftwareInstallAdobeReader -outfile c:\temp\downloads\adobereader.zip -Verbose
+Invoke-WebRequest $SoftwareInstallChrome -outfile c:\temp\downloads\chrome.msi -Verbose
+Invoke-WebRequest $SoftwareInstall7zip -outfile c:\temp\downloads\7zip.msi -Verbose
+Invoke-WebRequest $SoftwareInstallJava -outfile c:\temp\downloads\java.msi -Verbose
+Invoke-WebRequest $SoftwareInstallAdobeReader -outfile c:\temp\downloads\adobereader.zip -Verbose
 Expand-Archive -LiteralPath c:\temp\downloads\adobereader.zip -DestinationPath c:\temp\downloads\ 
 $ProgressPreference = 'Continue'
 Start-Process msiexec.exe -Wait -ArgumentList '/i c:\temp\downloads\chrome.msi /qn /norestart allusers=2'
