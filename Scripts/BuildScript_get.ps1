56a3a71f731ce574fa47d426df0d910dab064b81
Mohammed Ramim Quashem <ramim_qr@mrqlab.co.uk>
2021-04-16 20:44:08 +0100

    remove extra arguments

--- Scripts/BuildScript_get.ps1
+++ Scripts/BuildScript_get.ps1
@@ -4,10 +4,10 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
   Start-Process powershell -Verb runAs -ArgumentList $arguments
   Break
 }
-mkdir c:\temp
-powershell -command Invoke-WebRequest "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/BuildScript.ps1" -outfile c:\temp\BuildScript.ps1
-powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
+mkdir c:\temp > $null 2>&1
+Invoke-WebRequest "https://github.com/Ray-MRQ/MRQ/raw/master/Scripts/BuildScript.ps1" -outfile c:\temp\BuildScript.ps1
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 powershell c:\temp\BuildScript.ps1
-powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Restricted -Force}"
-Remove-Item c:\temp\BuildScript.ps1
+Set-ExecutionPolicy -ExecutionPolicy Restricted -Force
+Remove-Item c:\temp\BuildScript.ps1 > $null 2>&1
 exit
