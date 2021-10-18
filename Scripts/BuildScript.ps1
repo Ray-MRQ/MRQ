54ab309b3eb9106bdc6d9ff908b5d3a717429423
Raÿ <ramim_qr@hotmail.co.uk>
2021-10-18 16:11:47 +0100

    remove jar

--- Scripts/BuildScript.ps1
+++ Scripts/BuildScript.ps1
@@ -73,7 +73,7 @@ start-officecheck
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Clear-Host
 choco install googlechrome 7zip silverlight -y --ignorechecksum
-choco install jre8 -PackageParameters "/exclude:32" -y --ignorechecksum
+#choco install jre8 -PackageParameters "/exclude:32" -y --ignorechecksum
 choco install adobereader -params '"/DesktopIcon"' -y --ignorechecksum
 Clear-Host
 do { $myInput = (Read-Host 'Would you like to install Zoom?(Y/N)').ToLower() } while ($myInput -notin @('y','n'))
