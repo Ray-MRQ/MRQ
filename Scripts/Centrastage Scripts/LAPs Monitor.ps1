36c3f66b05c0c2cf337319cd3ef1a9fd98ee29f0
Raÿ <ramim_qr@hotmail.co.uk>
2022-02-26 18:34:38 +0000

    fix some errors

--- Scripts/Centrastage Scripts/LAPs Monitor.ps1
+++ Scripts/Centrastage Scripts/LAPs Monitor.ps1
@@ -1,15 +1,3 @@
-function write-DRMMDiag ($messages) {
-    write-host  '<-Start Diagnostic->'
-    foreach ($Message in $Messages) { $Message }
-    write-host '<-End Diagnostic->'
-} 
-
-function write-DRRMAlert ($message) {
-    write-host '<-Start Result->'
-    write-host "Alert=$message"
-    write-host '<-End Result->'
-}
-
 #DATTO VARIABLES  ---------------------------------------------------------------------------
 
 $varLocalHost = Hostname
@@ -22,21 +10,27 @@ $RunOnServer = "true"
 
 #FUNCTIONS START ---------------------------------------------------------------------------
 
-Try {
+function write-DRRMAlert ($message) {
+    write-host '<-Start Result->'
+    write-host "Alert=$message"
+    write-host '<-End Result->'
+}
+
 function Get-RandomCharacters($length, $characters) {
     $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
     $private:ofs=""
     return [String]$characters[$random]
 }
- 
-function Scramble-String([string]$inputString){     
-    $characterArray = $inputString.ToCharArray()   
-    $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length     
+
+function Scramble-String([string]$inputString){
+    $characterArray = $inputString.ToCharArray()
+    $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length
     $outputString = -join $scrambledStringArray
-    return $outputString 
+    return $outputString
 }
 
 #SCRIPT START ---------------------------------------------------------------------------
+
 if ($RunOnServer -eq $false){
 $OSCheck = (Get-WmiObject -Class Win32_OperatingSystem).ProductType
 
@@ -66,12 +60,12 @@ $varCharList = "abcdefghijkmnopqrstuvwxzyABCEEFGHJKLMNOPQRSTUVWXYZ1234567890"
 $varPassLength = Get-Random -Minimum $MinPassLength -Maximum $MaxPassLength
 
 #Uses random length and adds 4 extra characters (To make sure it has 1 upper case, 1 lower case, 1 number and a symbol)
-$varPassword = Get-RandomCharacters -length $varPassLength -characters $varCharList 
+$varPassword = Get-RandomCharacters -length $varPassLength -characters $varCharList
 $varPassword += Get-RandomCharacters -length 1 -characters 'abcdefghikmnoprstuvwxyz'
 $varPassword += Get-RandomCharacters -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
 $varPassword += Get-RandomCharacters -length 1 -characters '1234567890'
 if ($IncludeSymbol -eq $true){$varPassword += Get-RandomCharacters -length 1 -characters '!£%*()_-'}
- 
+
 $varPassword = Scramble-String $varPassword
 
 $varUserAccount = gwmi -class Win32_UserAccount | Where Domain -eq $varLocalHost | Where Name -eq $varAdminAccount
@@ -89,14 +83,15 @@ if ($varUserAccount.Name -eq $varAdminAccount) {
     net user $varAdminAccount /expires:never | Out-Null
     net user $varAdminAccount /fullname:$varAdminAccount | Out-Null
     net localgroup administrators $varAdminAccount /add | Out-Null
-    Set-ItemProperty -Path HKLM:\SOFTWARE\CentraStage -Name $UDFSelection -Type String -Value $varPassword -Force | Out-Null
+
     }
-Remove-Item "C:\Program Files (x86)\CentraStage\log.txt" -force
+
+
+$D = Date -Format F
+$SD = "Last updated on:"
+$RD = "$VARPASSWORD" + " " + $SD + " " + $D
+
 write-DRRMAlert "Healthy: Password set."
-Exit 0
-}
-catch {
-    write-DRMMDiag $($_.Exception.Message)
-    write-DRRMAlert "Unhealthy: Check Diagonstic info."
-Exit 1
-}
+Set-ItemProperty -Path HKLM:\SOFTWARE\CentraStage -Name $UDFSelection -Type String -Value "$RD" -Force | Out-Null
+Start-Sleep -s 60
+Remove-Item "C:\Program Files (x86)\CentraStage\log.txt" -force
