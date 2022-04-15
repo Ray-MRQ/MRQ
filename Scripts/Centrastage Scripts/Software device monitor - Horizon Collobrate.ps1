b00f02b73990a22414d27d4db4cbe9ffd168ffa0
Raÿ <ramim_qr@hotmail.co.uk>
2022-04-15 17:15:07 +0100

    update for cleaner process as it wasn't working properly

--- Scripts/Centrastage Scripts/Software device monitor - Horizon Collobrate.ps1
+++ Scripts/Centrastage Scripts/Software device monitor - Horizon Collobrate.ps1
@@ -1,34 +1,38 @@
 If(-not(Get-InstalledModule RunAsUser -ErrorAction silentlycontinue)){
     Install-Module RunAsUser -Confirm:$False -Force
 }
-mkdir c:\software > $null 2>&1
 
 $checkscript = {
-Copy-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Horizon Collaborate.lnk" "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\Horizon.lnk" -Force
-test-path $env:localappdata\Programs\collaborate-evolution
-}
+if (test-path "$env:localappdata\Programs\collaborate-evolution\Horizon Collaborate.exe") {
+    Copy-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Horizon Collaborate.lnk" "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\Horizon.lnk" -Force
+} else {
+    set-content c:\temp\horizon_not_installed.txt notinstalled
+}}
 
-$Check = invoke-ascurrentuser -scriptblock $checkscript
+invoke-ascurrentuser -scriptblock $checkscript
+
+$Check = test-path c:\temp\horizon_not_installed.txt
 
 if($Check -eq "true") {
-write-host '<-Start Result->'
-Write-Host "STATUS='Horizon Collobrate' installed."
-write-host '<-End Result->'
-Start-Sleep -Seconds 30
-Remove-Item c:\software\Horizon.exe -force > $null 2>&1
-Exit 0
+    write-host '<-Start Result->'
+    Write-Host "STATUS='Horizon Collobrate' not installed."
+    write-host '<-End Result->'
+    $Link = $env:horizondownload
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest $Link -outfile "c:\temp\Horizon.exe"
+    $ProgressPreference = 'Continue'
+    $scriptblock = {C:\temp\Horizon.exe} 
+    invoke-ascurrentuser -scriptblock $scriptblock
+    Start-Sleep -Seconds 30
+    Remove-Item c:\temp\Horizon.exe -force > $null 2>&1
+    Remove-Item c:\temp\horizon_not_installed.txt -force > $null 2>&1
+    Exit 1
 } else {
-write-host '<-Start Result->'
-Write-Host "STATUS='Horizon Collobrate' not installed."
-write-host '<-End Result->'
-Start-Sleep -Seconds 30
-$Link = $env:horizondownload
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest $Link -outfile "c:\software\Horizon.exe"
-$ProgressPreference = 'Continue'
-$scriptblock = {
-If(test-path $env:localappdata\Programs\collaborate-evolution) {} else {C:\software\Horizon.exe}} 
-invoke-ascurrentuser -scriptblock $scriptblock
-Remove-Item c:\software\Horizon.exe -force > $null 2>&1
-Exit 1
+    write-host '<-Start Result->'
+    Write-Host "STATUS='Horizon Collobrate' installed."
+    write-host '<-End Result->'
+    Start-Sleep -Seconds 30
+    Remove-Item c:\temp\horizon_not_installed.txt -force > $null 2>&1
+    Remove-Item c:\temp\Horizon.exe -force > $null 2>&1
+    Exit 0
 }
